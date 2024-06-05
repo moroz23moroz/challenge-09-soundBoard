fc162b0b931a5c916e5632b0168a29bc4ebffec6
Marina Moroz <moroz2387moroz@yandex.ru>
2024-06-05 14:30:56 +1000

    add styles of buttons

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,22 @@ window.addEventListener("DOMContentLoaded", () => {
     "robo",
     "wrong",
   ];
+
   sounds.forEach((sound) => {
     const bts = document.createElement("button");
     bts.classList.add("bts");
     bts.innerText = sound;
+    bts.addEventListener("click", () => {
+      stopSongs();
+      document.getElementById(sound).play();
+    });
     document.getElementById("buttons").appendChild(bts);
   });
+  function stopSongs() {
+    sounds.forEach((sound) => {
+      const single = document.getElementById(sound);
+      single.pause();
+      single.currentTime = 0;
+    });
+  }
 });

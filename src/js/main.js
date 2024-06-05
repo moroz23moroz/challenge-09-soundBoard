c53e0f651d841956556bc91de39650ccde5d603c
Marina Moroz <moroz2387moroz@yandex.ru>
2024-06-05 13:47:38 +1000

    add script

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
-
+window.addEventListener("DOMContentLoaded", () => {
+  const sounds = [
+    "applause",
+    "camera",
+    "crowd",
+    "electricity",
+    "robo",
+    "wrong",
+  ];
+  sounds.forEach((sound) => {
+    const bts = document.createElement("button");
+    bts.classList.add("bts");
+    bts.innerText = sound;
+    document.getElementById("buttons").appendChild(bts);
+  });
+});
